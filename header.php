5acdb3ed3e6a1402000630e32ae724e6dacbc078
Ram <maricarreyes111699@gmail.com>
2021-12-28 18:15:38 +0800

    [MOD] Remove BG

--- header.php
+++ header.php
@@ -1,41 +1,13 @@
 <!DOCTYPE html>
-<html class="no-js">
-    <head>
-        <title>BSU - SC Homepage</title>
-
-     <style>
-		body{
-       		 background: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6));
-    	}
-    	
-    .bsutitle{
-        color: white;
-        margin-left: 20px    ;
-
-    }
-	
-		</style>
-<link rel="shortcut icon" type="image/png" href="LOGO.png">
-		<meta charset="UTF-8">
-        <!-- Bootstrap -->
-				<link href="admin/images/favicon.ico" rel="icon" type="image">
-				<link href="admin/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"/>
-				<link href="admin/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen"/>
-				<link href="admin/bootstrap/css/font-awesome.css" rel="stylesheet" media="screen"/>
-				<link href="admin/bootstrap/css/my_style.css" rel="stylesheet" media="screen"/>
-				<link href="admin/vendors/easypiechart/jquery.easy-pie-chart.css" rel="stylesheet" media="screen"/>
-				<link href="admin/assets/styles.css" rel="stylesheet" media="screen"/>
-				<!-- calendar css -->
-				<link href="admin/vendors/fullcalendar/fullcalendar.css" rel="stylesheet" media="screen">
-				<!-- index css -->
-				<link href="admin/bootstrap/css/index.css" rel="stylesheet" media="screen"/>
-				<!-- data table css -->
-				<link href="admin/assets/DT_bootstrap.css" rel="stylesheet" media="screen"/>
-				<!-- notification  -->
-				<link href="admin/vendors/jGrowl/jquery.jgrowl.css" rel="stylesheet" media="screen"/>
-				<!-- wysiwug  -->
-				<link rel="stylesheet" type="text/css" href="admin/vendors/bootstrap-wysihtml5/src/bootstrap-wysihtml5.css"/>
-		<script src="admin/vendors/jquery-1.9.1.min.js"></script>
-        <script src="admin/vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-    </head>
-<?php include('admin/dbcon.php'); ?>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<p>Hello World
+	</p>
+</body>
+</html>
