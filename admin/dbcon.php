962ad7fa9142c59760e2218935ab7ba6ea44c541
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-01-05 01:31:05 +0800

    [MOD] Change database connection

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect('localhost','root','','capstone') or die(mysqli_error());
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net','bedcae9a9caadd','650a431e','heroku_7325063326eb664') or die(mysqli_error());
 ?>
