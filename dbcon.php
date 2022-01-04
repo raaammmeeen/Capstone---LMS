ffe8dba12f666441ec50ce95f1973a5bd8d5a6ab
raaammmeeen <84072934+raaammmeeen@users.noreply.github.com>
2022-01-04 16:25:06 +0800

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect('localhost','root','','capstone') or die(mysqli_error());
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net','bedcae9a9caadd','650a431e','mysql://bedcae9a9caadd:650a431e@us-cdbr-east-05.cleardb.net/heroku_7325063326eb664?reconnect=true') or die(mysqli_error());
 ?>
