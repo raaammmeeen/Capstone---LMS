423ea26c8b61da7038ffbaaf41871fbf2b95df4c
raaammmeeen <84072934+raaammmeeen@users.noreply.github.com>
2022-01-05 00:26:57 +0800

    Update session.php

--- session.php
+++ session.php
@@ -3,7 +3,7 @@
 session_start();
 //Check whether the session variable SESS_MEMBER_ID is present or not
 if (!isset($_SESSION['id']) || ($_SESSION['id'] == '')) {
-    header("location: index.php");
+    header("location: index.html");
     exit();
 }
 
