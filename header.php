5d88cb610fb5fcbd4869c6e420c96c9db55a0ce0
Ram <maricarreyes111699@gmail.com>
2022-01-08 05:32:35 +0800

    [ADD] Background picture

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 	     <link rel="shortcut icon" type="image/png" href="LOGO.png">
  	<style>
 		body{
-       		 	background: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)), url("admin/images/bsusc 1.jpeg");
+       		 	background: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)), url("admin/images/bsusc 1.png");
     		}
     	
     		.bsutitle{
