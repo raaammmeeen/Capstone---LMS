c25d9599af0f366b82ed22c97f5371a2192ac0fb
Ram <maricarreyes111699@gmail.com>
2022-01-17 10:19:23 +0800

    [MOD] Show changes announcement_student

--- announcements_student.php
+++ announcements_student.php
@@ -10,7 +10,7 @@
                      <div class="row-fluid">
 					  <!-- breadcrumb -->
 				
-										<?php $class_query = mysqli_query($conn,"select * from teacher_class
+					  <?php $class_query = mysqli_query($conn,"select * from teacher_class
 										LEFT JOIN class ON class.class_id = teacher_class.class_id
 										LEFT JOIN subject ON subject.subject_id = teacher_class.subject_id
 										where teacher_class_id = '$get_id'")or die(mysqli_error());
@@ -20,6 +20,7 @@
 					     <ul class="breadcrumb">
 							<li><a href="#"><?php echo $class_row["class_name"]; ?></a> <span class="divider">/</span></li>
 							<li><a href="#"><?php echo $class_row["subject_code"]; ?></a> <span class="divider">/</span></li>
+							<li><a href="#">School Year: <?php echo $class_row['school_year']; ?></a> <span class="divider">/</span></li>
 							<li><a href="#"><b>Announcements</b></a></li>
 						</ul>
 						 <!-- end breadcrumb -->
