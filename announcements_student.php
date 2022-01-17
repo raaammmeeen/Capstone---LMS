6fde8846f4f1653240efbb12a75c42bbc8b2713a
Ram <maricarreyes111699@gmail.com>
2022-01-17 10:29:32 +0800

    [MOD] UNDO all changes announcement_student

--- announcements_student.php
+++ announcements_student.php
@@ -10,7 +10,18 @@
                      <div class="row-fluid">
 					  <!-- breadcrumb -->
 				
-									
+										<?php $class_query = mysqli_query($conn,"select * from teacher_class
+										LEFT JOIN class ON class.class_id = teacher_class.class_id
+										LEFT JOIN subject ON subject.subject_id = teacher_class.subject_id
+										where teacher_class_id = '$get_id'")or die(mysqli_error());
+										$class_row = mysqli_fetch_array($class_query);
+										?>
+				
+					     <ul class="breadcrumb">
+							<li><a href="#"><?php echo $class_row["class_name"]; ?></a> <span class="divider">/</span></li>
+							<li><a href="#"><?php echo $class_row["subject_code"]; ?></a> <span class="divider">/</span></li>
+							<li><a href="#"><b>Announcements</b></a></li>
+						</ul>
 						 <!-- end breadcrumb -->
 					 
                         <!-- block -->
@@ -21,7 +32,30 @@
                             </div>
                             <div class="block-content collapse in">
                                 <div class="span12">
-								
+								 <?php
+								 $query_announcement = mysqli_query($conn,"select * from teacher_class_announcements
+																	where  teacher_class_id = '$get_id' order by date DESC
+																	")or die(mysqli_error());
+								$count = mysqli_num_rows($query_announcement);
+								if ($count > 0){
+								 while($row = mysqli_fetch_array($query_announcement)){
+								 $id = $row["teacher_class_announcements_id"];
+								 ?>
+											<div class="post"  id="del<?php echo $id; ?>">
+											<?php echo $row['content']; ?>
+										
+											<hr>
+											
+										
+											<strong><i class="icon-calendar"></i> <?php echo $row['date']; ?></strong>
+											
+										
+											
+											</div>
+											
+								<?php }}else{ ?>
+								<div class="alert alert-info"><i class="icon-info-sign"></i> No Announcements Found.</div>
+								<?php } ?>
                                 </div>
 								
 							
