db4d59d3b999ba202c34e76757220bf8272b16bf
Ram <maricarreyes111699@gmail.com>
2022-01-25 16:14:25 +0800

    [MOD] Fixed student notification read

--- read.php
+++ read.php
@@ -6,11 +6,7 @@ $id=$_POST['selector'];
 $N = count($id);
 for($i=0; $i < $N; $i++)
 {
-
 	mysqli_query($conn,"insert into notification_read (student_id,student_read,notification_id) values('$session_id','yes','$id[$i]')")or die(mysqli_error());
-	
-	
-	
 }
 ?>
 <script>
