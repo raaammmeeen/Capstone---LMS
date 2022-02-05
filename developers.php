9bd6f88b85faf7d7cc8fcf8f2ddef1ff13fc6176
Ram <maricarreyes111699@gmail.com>
2022-02-05 13:17:58 +0800

    [ADD] New pic

--- developers.php
+++ developers.php
@@ -15,7 +15,7 @@
 							<hr>
                                 <div class="span3">
 										<center>
-										<img id="developers" src="admin/images/editable.png" class="img-circle">
+										<img id="developers" src="admin/images/ram (2).PNG" class="img-circle">
 										<hr>
 										<p>Name: Maricar B. Reyes</p>
 										<p>Address: City of San Jose Del Monte, Bulacan</p>
